e98c51f6bffe5d8ef029340d0751a73f8187e8a0
Ujjawal <ujjawal@thoughtnirvana.com>
2013-05-22 00:50:54 +0530

    devise files and chaning application erb to haml

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ TheBookDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'user#index'
 
   # See how all your routes lay out with "rake routes"
 
