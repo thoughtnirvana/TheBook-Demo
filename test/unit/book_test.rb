d4ab53aa99a48cda25a1aa811de3e5a4d0192be2
Ujjawal <ujjawal@thoughtnirvana.com>
2013-05-22 15:31:24 +0530

    test files

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  author     :string(255)
+#  isbn       :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
--- test/unit/rating_test.rb
+++ test/unit/rating_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id         :integer          not null, primary key
+#  book_id    :integer
+#  user_id    :integer
+#  rating     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class RatingTest < ActiveSupport::TestCase
